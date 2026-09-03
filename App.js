f6b9fff9ce2e0be1151b522554ba27abbd5497fb
cuzimapanda2003 <marcblais03@gmail.com>
2026-09-03 15:56:51 +0000

    finished UI 2 + started UI 3

--- App.js
+++ App.js
@@ -1,31 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, TextInput, Button, Text, Pressable} from 'react-native';
-
+import RadioGroup from 'react-native-radio-buttons-group';
+import { Picker } from '@react-native-picker/picker';
 
 
 function Field({label,})
 {
   return(
   <View>
-    <TextInput style = { {borderColor: 'white', borderWidth: 1, margin: 10, color: 'white',} } placeholder={label}></TextInput>
+    <TextInput style = { {borderColor: 'white', borderWidth: 1, margin: 10, color: 'white', height: 30, paddingLeft: 5,} } placeholder={label}></TextInput>
   </View>
   )
 }
 
-
-export default function App() {
+// UI 1
+function LoginPage()
+{
   const fields = [
     'Username',
     'Password',
   ]
-
    return (
       <View style={[styles.container ,{alignItems: 'center',}] }> 
   
         {fields.map(f => <Field label={f}/>)}
 
-        <Button title='login' color={"orange"}/>
-        
+        <View style={{paddingTop: 15}}>
+            <Button title='login' color={"orange"}/>
+        </View>
+
         <Pressable>
         <Text style={ {color: 'blue',marginTop: 50} }>Sign up!</Text>
         </Pressable>
@@ -33,6 +36,76 @@ export default function App() {
       </View>
     )
 }
+
+// UI 2
+function SignupPage(){
+   const fields = [
+    'Username',
+    'Password',
+    'Password confirmation'
+  ]
+   return (
+      <View style={[styles.container ,{alignItems: 'center',}] }> 
+  
+        {fields.map(f => <Field label={f}/>)}
+
+        <View style={{paddingTop: 15}}>
+           <Button title='Create my account' color={"orange"}/>
+        </View>
+  
+      </View>
+    )
+}
+
+// UI 3
+function RecettePage(){
+      const options = [
+        {
+            id: '1',
+            label: 'Breakfast',
+            value: '1'
+        },
+        {
+            id: '2',
+            label: 'Lunch',
+            value: '2'
+        },
+        {
+          id: '3',
+          label: 'Dinner',
+          value: '3'
+        },
+    ];
+
+     return (
+
+        <View style={[styles.container ,{justifyContent: 'top',}] } >
+            <RadioGroup radioButtons={ options } layout='row'/>
+             <TextInput style = { {borderColor: 'white', borderWidth: 1, margin: 10, color: 'white', height: 30,width: '90%' ,paddingLeft: 5,} } placeholder='Name'></TextInput>
+        </View>
+  
+
+  
+    )
+
+
+
+}
+
+export default function App() {
+
+   return (
+      <View style={[styles.container ,{alignItems: 'center',}] }> 
+      {/* <LoginPage/> */}
+      {/* <SignupPage/> */}
+      <RecettePage/>
+      
+  
+      </View>
+    )
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
